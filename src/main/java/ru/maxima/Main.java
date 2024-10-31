fc95c73107d0e143c90bf18eecdbc57ac6f72de4
viktoriatsenko <nashtrenerishka@gmail.com>
2024-10-31 20:24:10 +0300

    annotations finish

--- src/main/java/ru/maxima/Main.java
+++ src/main/java/ru/maxima/Main.java
@@ -20,9 +20,6 @@ public class Main {
         RadioPlayer player = context.getBean("player", RadioPlayer.class);
         RadioPlayer player1 = context.getBean("player", RadioPlayer.class);
 
-
-
-
         player.play();
 
         System.out.println(player == player1);
--- src/main/java/ru/maxima/config/SpringConfig.java
+++ src/main/java/ru/maxima/config/SpringConfig.java
@@ -1,11 +1,20 @@
 package ru.maxima.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import ru.maxima.di_radio.RadioPlayer;
 
 @Configuration
 @ComponentScan("ru.maxima")
 @PropertySource("classpath:player.properties") // class path это путь к папке main
 public class SpringConfig {
+
+    @Bean
+    public RadioPlayer radioPlayer() {
+       return new RadioPlayer();
+    }
+
+
 }
